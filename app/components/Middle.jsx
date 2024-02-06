511289d3d511caea0c550d27a9d825d1fb029757
jacklocks <castets.eric@free.fr>
2024-02-06 15:45:57 +0100

    middle2

--- app/components/Middle.jsx
+++ app/components/Middle.jsx
@@ -9,42 +9,102 @@ const Middle = () => {
         voluptate voluptatibus eaque magnam deserunt ipsum alias nostrum!
         Cupiditate, ipsam? Ex, hic?
       </h1>
-      <div className="flex place-content-around mt-20 flex-wrap gap-40">
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-            <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+      <div className="flex place-content-around mt-20 flex-wrap gap-20">
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded mb-10">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-        <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-        <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-        <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-        <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
-        <div className="bg-turquoise w-96 h-[35rem] rounded">
-        <h1 className="text-3xl text-center p-5">lorem</h1>
-            <p className="p-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem inventore atque consequatur asperiores rem magni dignissimos minus sit ex adipisci error, officiis nisi ipsam quo? Magni quae soluta a et.
-            Perspiciatis repudiandae dolor ipsa quas eos rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea. Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi sequi nemo dolorem ullam fuga voluptatem.
-            Ratione dicta, hic minima ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt rem repellendus explicabo ducimus qui iusto assumenda asperiores facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis rem?</p>
+        <div className="bg-turquoise w-[25rem] h-[35rem] rounded">
+          <h1 className="text-4xl text-center p-5">lorem</h1>
+          <p className="p-5 text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem inventore atque consequatur asperiores rem magni
+            dignissimos minus sit ex adipisci error, officiis nisi ipsam quo?
+            Magni quae soluta a et. Perspiciatis repudiandae dolor ipsa quas eos
+            rem pariatur assumenda nihil laudantium cum mollitia, minus nemo ea.
+            Distinctio minus iusto excepturi! Veritatis aliquam quae excepturi
+            sequi nemo dolorem ullam fuga voluptatem. Ratione dicta, hic minima
+            ipsa repellat nesciunt numquam fugiat repudiandae voluptates sunt
+            rem repellendus explicabo ducimus qui iusto assumenda asperiores
+            facere quae ipsum amet accusamus. Explicabo nulla quibusdam corporis
+            rem?
+          </p>
         </div>
       </div>
     </section>
